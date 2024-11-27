f080a00117919b197a31b49842453704982a1e9b
iDavLaFF <idavlaff@ya.ru>
2024-11-28 01:44:47 +0300

    Update WEB.js

--- WEB.js
+++ WEB.js
@@ -7,10 +7,12 @@
         'https://idavlaff.github.io/lampa/plugins/timecode.js',
         'https://lampame.github.io/main/pubtorr/pubtorr.js',
         'https://lampame.github.io/main/nc/nc.js',
-        'https://nb557.github.io/plugins/rating.js',
+ /* Off 'KPrating' ! don't work with 'cardify', need update.
+        'https://nb557.github.io/plugins/rating.js', */
         'https://idavlaff.github.io/lampa/plugins/interfacify.js',
-//        'https://idavlaff.github.io/lampa/plugins/interface.js',
-//        'https://idavlaff.github.io/lampa/plugins/cardify.js',
+/* Combine 'interface' & 'cardify' plugins => 'interfacify' ! 'cardify' without trailers, only interface beauty. 
+        'https://idavlaff.github.io/lampa/plugins/interface.js',
+        'https://idavlaff.github.io/lampa/plugins/cardify.js', */
         'https://idavlaff.github.io/lampa/plugins/inquality.js',
         'https://idavlaff.github.io/lampa/plugins/cuboff.js'
     ], function () { });
