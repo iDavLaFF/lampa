341498dd9a0aa567a7abcd5173500c15dd28b5fc
iDavLaFF <idavlaff@ya.ru>
2024-11-27 19:36:01 +0300

    Update idavlaff-lampa-plugins.js

--- idavlaff-lampa-plugins.js
+++ idavlaff-lampa-plugins.js
@@ -8,7 +8,8 @@
     	'https://lampame.github.io/main/nc/nc.js',
         'https://nb557.github.io/plugins/rating.js',
         'https://idavlaff.github.io/lampa/plugins/interface.js',
-        'https://idavlaff.github.io/lampa/plugins/cardify.js',
+/* Cardify freezing 'LampaTV.app' startup on 'Loader init' 
+        'https://idavlaff.github.io/lampa/plugins/cardify.js', */
         'https://idavlaff.github.io/lampa/plugins/inquality.js',
         'https://idavlaff.github.io/lampa/plugins/cuboff.js'
     ], function () { });
