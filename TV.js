0d4f24f754012874f729bff7c74710c25cbee621
iDavLaFF <idavlaff@ya.ru>
2024-11-27 22:27:40 +0300

    Create TV.js

---
+++ TV.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    Lampa.Utils.putScriptAsync([
+        'http://cub.red/plugin/tmdb-proxy',
+        'http://cub.red/plugin/tracks'/*,
+        'https://idavlaff.github.io/lampa/plugins/timecode.js',
+        'https://lampame.github.io/main/pubtorr/pubtorr.js',
+        'https://lampame.github.io/main/nc/nc.js',
+        'https://nb557.github.io/plugins/rating.js',
+        'https://idavlaff.github.io/lampa/plugins/interface.js',
+        'https://idavlaff.github.io/lampa/plugins/cardifyby.js',
+        'https://idavlaff.github.io/lampa/plugins/inquality.js',
+        'https://idavlaff.github.io/lampa/plugins/cuboff.js' */
+    ], function () { });
+
+})();
