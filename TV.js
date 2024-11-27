228ecb860bfc37f3fa9c0c6bd632350e23463f9b
iDavLaFF <idavlaff@ya.ru>
2024-11-27 22:45:50 +0300

    Update TV.js

--- TV.js
+++ TV.js
@@ -9,7 +9,7 @@
         'https://lampame.github.io/main/nc/nc.js',
         'https://nb557.github.io/plugins/rating.js',
         'https://idavlaff.github.io/lampa/plugins/interface.js',
-        'https://idavlaff.github.io/lampa/plugins/cardifyby.js'/*,
+        'https://idavlaff.github.io/lampa/plugins/cardify.js'/*,
         'https://idavlaff.github.io/lampa/plugins/inquality.js',
         'https://idavlaff.github.io/lampa/plugins/cuboff.js' */
     ], function () { });
