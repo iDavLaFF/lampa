ec5f9db52a9219729e74551b80d41f9b970ac64a
iDavLaFF <idavlaff@ya.ru>
2024-11-27 20:04:23 +0300

    Update idavlaff-lampa-plugins.js

--- idavlaff-lampa-plugins.js
+++ idavlaff-lampa-plugins.js
@@ -3,6 +3,7 @@
 
     Lampa.Utils.putScriptAsync([
         'http://cub.red/plugin/tmdb-proxy',
+        'http://cub.red/plugin/tracks',
         'https://idavlaff.github.io/lampa/plugins/timecode.js',
         'https://lampame.github.io/main/pubtorr/pubtorr.js',
         'https://lampame.github.io/main/nc/nc.js',
