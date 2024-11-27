53c91e430aeace5132fb7ce518538c5b0edd5204
iDavLaFF <idavlaff@ya.ru>
2024-11-28 01:28:58 +0300

    Update WEB.js

--- WEB.js
+++ WEB.js
@@ -8,8 +8,9 @@
         'https://lampame.github.io/main/pubtorr/pubtorr.js',
         'https://lampame.github.io/main/nc/nc.js',
         'https://nb557.github.io/plugins/rating.js',
-        'https://idavlaff.github.io/lampa/plugins/interface.js',
-        'https://idavlaff.github.io/lampa/plugins/cardify.js',
+        'https://idavlaff.github.io/lampa/plugins/interfacify.js',
+//        'https://idavlaff.github.io/lampa/plugins/interface.js',
+//        'https://idavlaff.github.io/lampa/plugins/cardify.js',
         'https://idavlaff.github.io/lampa/plugins/inquality.js',
         'https://idavlaff.github.io/lampa/plugins/cuboff.js'
     ], function () { });
