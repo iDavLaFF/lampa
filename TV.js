c657c50235f90c1d06adee3dabb554c8f432d312
iDavLaFF <idavlaff@ya.ru>
2024-11-27 22:38:05 +0300

    Update TV.js

--- TV.js
+++ TV.js
@@ -5,9 +5,9 @@
         'http://cub.red/plugin/tmdb-proxy',
         'http://cub.red/plugin/tracks',
         'https://idavlaff.github.io/lampa/plugins/timecode.js',
-        'https://lampame.github.io/main/pubtorr/pubtorr.js',/*
+        'https://lampame.github.io/main/pubtorr/pubtorr.js',
         'https://lampame.github.io/main/nc/nc.js',
-        'https://nb557.github.io/plugins/rating.js',
+        'https://nb557.github.io/plugins/rating.js',/*
         'https://idavlaff.github.io/lampa/plugins/interface.js',
         'https://idavlaff.github.io/lampa/plugins/cardifyby.js',
         'https://idavlaff.github.io/lampa/plugins/inquality.js',
