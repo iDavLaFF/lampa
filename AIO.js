fba3ada5643ea8589cb30aceee68dc505fe51294
iDavLaFF <idavlaff@ya.ru>
2024-12-24 17:47:29 +0300

    Update AIO.js

--- AIO.js
+++ AIO.js
@@ -8,15 +8,16 @@
 // @LME thanks. Public parsers
         'https://lampame.github.io/main/pubtorr/pubtorr.js',
 // @LME thanks. new categories & collection button for 'franchises'
-        'https://lampame.github.io/main/nc/nc.js',
+//        'https://lampame.github.io/main/nc/nc.js',
 // @LME thanks. torrents to Infuse saver (iOS/macOS). !NB 'Save all' need 'shortcut': https://www.icloud.com/shortcuts/406d7f6bdfcf466da153a38dea6bb663 
         'https://lampame.github.io/main/its/its.js',
 // @NB557 thanks. 'KPrating.plugin' !NB Need update 'cause not work with 'cardify'
-//        'https://nb557.github.io/plugins/rating.js',
+        'https://nb557.github.io/plugins/rating.js',
 // @LAMPAC thanks. !NB Timecode work only in torrents
         'https://idavlaff.github.io/lampa/plugins/timecode.js',
 // @BWA thanks. Add 'Strawberry.plugin'
         'https://idavlaff.github.io/lampa/plugins/sisi.js?v=' + Math.random(),
+        'https://idavlaff.github.io/lampa/plugins/modification.js?v=' + Math.random(),
 // @CUB thanks, !'but pay for a big background, seriously?' Combine 'interface' & 'cardify' plugins => 'beautify' !NB 'cardify' without trailers, only cards beauty
         'https://idavlaff.github.io/lampa/plugins/beautify.js?v=' + Math.random(),
 // Add new category "4K releases"
