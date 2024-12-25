0419b4a086e38990dd03db97276c581aad127f98
iDavLaFF <idavlaff@ya.ru>
2024-12-26 02:40:29 +0300

    Update mult.js

--- plugins/menu/mult.js
+++ plugins/menu/mult.js
@@ -25,7 +25,7 @@
     });
     $('.menu .menu__list').eq(0).append(button);
     setTimeout(function () {
-      $('[data-action="mult"]').insertBefore($('[data-action="catalog"]'));
+      $('[data-action="mult"]').insertAfter($('[data-action="tv"]'));
     }, 2000);
   }
 
