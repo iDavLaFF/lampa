a7eb8d6ea4843b7db6b8d11d2a42ab53342d3103
iDavLaFF <idavlaff@ya.ru>
2024-12-26 01:40:53 +0300

    Update LU.js

--- LU.js
+++ LU.js
@@ -22,7 +22,7 @@
 // @CUB thanks, !'but pay for a big background, seriously?' Combine 'interface' & 'cardify' plugins => 'beautify' !NB 'cardify' without trailers, only cards beauty
         'https://idavlaff.github.io/lampa/plugins/beautify.js',
 // Add new category "4K releases"
-        'https://idavlaff.github.io/lampa/plugins/new4k.js'
+        'https://idavlaff.github.io/lampa/plugins/menu/new4k.js'
     ], function () { });
 
 })();
