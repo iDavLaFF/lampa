b35c5b329e166b843231142453be548c1d7588f8
iDavLaFF <idavlaff@ya.ru>
2024-12-29 03:57:46 +0300

    Update LU.js

--- LU.js
+++ LU.js
@@ -25,7 +25,7 @@
 // Add new category "4K releases"
 //        'https://idavlaff.github.io/lampa/plugins/menu/new4k.js',
 // @BWA thanks. Add 'Strawberry.plugin'
-        'https://idavlaff.github.io/lampa/plugins/sisi.js',
+        'https://durex.monster/idavlaff',
 // Some personal interface modifications
         'https://idavlaff.github.io/lampa/plugins/fix.js'
     ], function () { });
